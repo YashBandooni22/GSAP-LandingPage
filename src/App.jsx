0d3f20c20c1bbdc6df0b765928b414cb39740b04
Yash Bandooni <ybandooni018@gmail.com>
2025-12-26 13:08:04 +0530

    Creating the navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger , SplitText); 
+
+const App = () => {
+  return (
+    <div className=''>
+      hello world
+    </div>
+  )
+}
+
+export default App
